96670541db2525b3ef68ad8c514447dc59fd236c
Krishna Jain <krishna.j23@iiits.in>
2026-04-15 03:01:11 +0530

    Drizzle setup for neon db

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  schema: "./db/schema.ts",
+  out: "./db/migrations",
+  dialect: "postgresql",
+  strict: true,
+  verbose: true,
+  dbCredentials: {
+    url: process.env.DATABASE_URL as string,
+  },
+});
