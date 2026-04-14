ee8ce2a923481247b845a398454bfacb438a3e3a
Krishna Jain <krishna.j23@iiits.in>
2026-04-15 02:51:24 +0530

    icon bug + userbutton on events

--- app/(private)/events/page.tsx
+++ app/(private)/events/page.tsx
@@ -1,3 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function EventsPage() {
-	return <h1>Hi</h1>;
+	return (
+		<main className="flex min-h-full flex-1 items-center justify-center">
+			<div className="flex items-center gap-3">
+				<h1>Hi</h1>
+				<UserButton />
+			</div>
+		</main>
+	);
 }
