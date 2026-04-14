bb29b28f8e6bf7cd42d54f1563b577ee678cca39
Krishna Jain <krishna.j23@iiits.in>
2026-04-15 03:14:47 +0530

    Events table

--- db/schema.ts
+++ db/schema.ts
@@ -1,1 +1,15 @@
-export {}
+import { boolean, integer, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+
+const createdAt = timestamp("createdAt").notNull().defaultNow();
+const updatedAt = timestamp("updatedAt").notNull().defaultNow().$onUpdate(() => new Date());
+
+export const EventTable = pgTable("events", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    name: text("name").notNull(),
+    description: text("description").notNull(),
+    durationInMinutes: integer("durationInMinutes").notNull(),
+    clerkUserId: text("clerkUserId").notNull(),
+    isActive : boolean("isActive").notNull().default(true),
+    createdAt,
+    updatedAt
+});
